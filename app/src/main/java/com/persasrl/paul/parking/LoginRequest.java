e1e6eb9faec9a86ed3d561b76d4f0acfdb60f80a
Persa Paul <ropaulsamp@gmail.com>
2016-09-07 14:26:53 +0300

    Rejected commit: Default

--- app/src/main/java/com/persasrl/paul/parking/LoginRequest.java
+++ app/src/main/java/com/persasrl/paul/parking/LoginRequest.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LoginRequest extends StringRequest {
-    private static final String LOGIN_REQUEST_URL = "http://parking.netau.net/Register.php";
+    private static final String LOGIN_REQUEST_URL = "http://parking.netau.net/Login.php";
     private Map<String, String> params;
 
     public LoginRequest(String username, String password, Response.Listener<String> listener) {
